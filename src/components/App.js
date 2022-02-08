86224610e3ff3df1346b4524592f435a48ff92d9
April Canete <aprilrose.canete93@gmail.com>
2022-02-08 11:54:10 +1100

    added api lyrics

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Lyrics from './Lyrics';
 
 function App() {
+  
+  
   return (
     <div className="App">
       <h1>jam app</h1>
+      <Lyrics />
     </div>
   );
 }
---
+++ src/components/Lyrics.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+export default function Lyrics() {
+    const [lyrics, setLyrics] = useState()
+    const APIKEY = "ab5d67aef8ebdd2357245607bc14dec9"
+
+    useEffect(() => {
+        fetchTrackID()
+    }, [])
+
+    const fetchTrackID = () => {
+        fetch(`/track.search?apikey=${APIKEY}&q_track=sorry&q_artist=justin bieber&f_has_lyrics=1`)
+        .then(res => res.json())
+        .then(res => res.message.body.track_list[0].track.track_id)
+        .then(trackID => fetchLyrics(trackID))
+        .catch((err)=>{
+            console.log(err.message)
+        })
+    }
+
+    const fetchLyrics = (trackID) => {
+        fetch(`track.lyrics.get?apikey=${APIKEY}&track_id=${trackID}`)
+        .then(res => res.json())
+        .then(res => setLyrics(res.message.body.lyrics.lyrics_body))
+        .catch((err)=>{
+            console.log(err.message)
+        })
+    }
+
+    return (
+        <div className="lyrics-box">
+          <p>{lyrics}</p>
+        </div>
+      );
+}
