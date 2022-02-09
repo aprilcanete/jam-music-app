0deb94eb15a00e0e6dce359697cbf400e9dea8f2
Matthew <mattwatkins000@hotmail.com>
2022-02-09 12:56:40 +1100

    added feature artist

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,7 +1,46 @@
 import './MusicPlayer.css'
+import React, { useState, useEffect } from 'react';
 import Lyrics from './Lyrics';
+import { Credentials } from './Credentials'
+import axios from 'axios';
 
-export default function MusicPlayer() {
+
+export default function MusicPlayer({ trackTitle }) {
+
+    const spotify = Credentials();  
+    const [token, setToken] = useState('');  
+    const [trackDetail, setTrackDetail] = useState({ });
+    
+
+    useEffect(() => {
+
+    axios('https://accounts.spotify.com/api/token', {
+        headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)      
+        },
+        data: 'grant_type=client_credentials',
+        method: 'POST'
+    })
+    .then(tokenResponse => {      
+        setToken(tokenResponse.data.access_token);
+
+        axios(`https://api.spotify.com/v1/search?q=${trackTitle}&type=track`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+        })
+        .then (songResponse => {        
+        setTrackDetail({
+            artist: songResponse.data.tracks.items[0].artists[0].name,
+            imageUrl: songResponse.data.tracks.items[0].album.images[1].url,
+            trackId: songResponse.data.tracks.items[0].id,
+            trackTitle: ''
+        })
+        });
+        
+    });
+
+    }, [spotify.ClientId, spotify.ClientSecret]);
 
     return (
         <div className="music-player-wrapper">
@@ -10,6 +49,7 @@ export default function MusicPlayer() {
             {/* <Lyrics /> */}
             <button className="lyrics-box">show lyrics</button>
             <p>play/pause</p>
+            <h1>{trackDetail.artist}</h1>
         </div>
     )
 }
